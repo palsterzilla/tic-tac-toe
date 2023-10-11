fd53e29a8c6f9993516baf8cee23954e0381674d
Firyal Ghassari <naufal.firyal@gmail.com>
2023-10-12 01:03:57 +0700

    Add game draw logic

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const X_CLASS = "x";
-const CIRCLE_CLASS = "circle"
+const CIRCLE_CLASS = "circle";
 const WINNING_COMBINATIONS = [
   [0, 1, 2],
   [3, 4, 5],
@@ -9,38 +9,57 @@ const WINNING_COMBINATIONS = [
   [2, 5, 8],
   [0, 4, 8],
   [2, 4, 6]
-]
-const cellElement = document.querySelectorAll("[data-cell]");
-const board = document.getElementById("board")
+];
+const cellElements = document.querySelectorAll("[data-cell]");
+const board = document.getElementById("board");
+const winningMessageElement = document.getElementById("winningMessage");
+const winningMessageTextElement = document.querySelector("[data-winning-message-text]");
 let circleTurn;
 
 startGame();
 
 function startGame() {
-  cellElement.forEach(cell => {
+  cellElements.forEach(cell => {
     cell.addEventListener("click", handleClick, { once: true });
   })
   setBoardHoverClass();
 }
-
 
 function handleClick(e) {
   const cell = e.target;
   const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
   placeMark(cell, currentClass);
   if (checkWin(currentClass)) {
-    alert("winner")
+    endGame(false);
+  } else if (isDraw()) {
+    endGame(true);
+  } else {
+    swapTurns();
+    setBoardHoverClass();
   }
-  swapTurns();
-  setBoardHoverClass()
+}
+
+function endGame(draw) {
+  if (draw) {
+    winningMessageTextElement.innerText = "Draw!";
+  } else {
+    winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`;
+  }
+  winningMessageElement.classList.add("show");
+}
+
+function isDraw() {
+  return [...cellElements].every(cell => {
+    return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+  })
 }
 
 function placeMark(cell, currentClass) {
-  cell.classList.add(currentClass)
+  cell.classList.add(currentClass);
 }
 
 function swapTurns() {
-  circleTurn = !circleTurn
+  circleTurn = !circleTurn;
 }
 
 function setBoardHoverClass() {
@@ -56,7 +75,7 @@ function setBoardHoverClass() {
 function checkWin(currentClass) {
   return WINNING_COMBINATIONS.some(combination => {
     return combination.every(index => {
-      return cellElement[index].classList.contains(currentClass)
+      return cellElements[index].classList.contains(currentClass)
     })
   })
 }
