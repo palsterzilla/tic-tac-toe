88b821922b9e5bac2bb39ef238af62a7811eadf4
Firyal Ghassari <naufal.firyal@gmail.com>
2023-10-12 01:07:40 +0700

    Add restart button handler

--- script.js
+++ script.js
@@ -14,15 +14,23 @@ const cellElements = document.querySelectorAll("[data-cell]");
 const board = document.getElementById("board");
 const winningMessageElement = document.getElementById("winningMessage");
 const winningMessageTextElement = document.querySelector("[data-winning-message-text]");
+const restartButton = document.getElementById("restartButton");
 let circleTurn;
+
+restartButton.addEventListener("click", startGame);
 
 startGame();
 
 function startGame() {
+  circleTurn = false;
   cellElements.forEach(cell => {
+    cell.classList.remove(X_CLASS);
+    cell.classList.remove(CIRCLE_CLASS);
+    cell.removeEventListener("click", handleClick);
     cell.addEventListener("click", handleClick, { once: true });
   })
   setBoardHoverClass();
+  winningMessageElement.classList.remove("show");
 }
 
 function handleClick(e) {
